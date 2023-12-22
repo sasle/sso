b6182f0672df0e215699cdc6ce28f224035d0f24
sasle <sasle57@gmail.com>
2023-12-22 00:15:34 -0300

    logout & user info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import LoginButton from "./components/login-button";
+import LogoutButton from "./components/logout-button";
+import Profile from "./components/profile";
 function App() {
-  return <LoginButton />;
+  return (
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <br />
+      <Profile />
+    </>
+  );
 }
 
 export default App;
